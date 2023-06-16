1983fe50bd216f9fd0c2e5e9c3e567843a1e3754
Adrian Tobi <talktotobi.a@gmail.com>
2023-06-16 10:39:05 +0100

    Deploy Please

--- app/app/page.js
+++ app/app/page.js
@@ -5,30 +5,30 @@ import AppContent from '@/components/AppContent'
 import prisma from '@/lib/client'
 
 export default async function Home() {
-  const spaces = await prisma.space.findMany()
+  // const spaces = await prisma.space.findMany()
 
-  const randomSpace = () => {
-    let randomNum = Math.floor(Math.random() * spaces.length)
-    return randomNum
-  }
+  // const randomSpace = () => {
+  //   let randomNum = Math.floor(Math.random() * spaces.length)
+  //   return randomNum
+  // }
 
-  const num = randomSpace()
-  const space = await prisma.space.findMany({
-    where: {
-      id: num,
-      // category: 'Anime',
-    }
-  })
+  // const num = randomSpace()
+  // const space = await prisma.space.findMany({
+  //   where: {
+  //     id: num,
+  //     // category: 'Anime',
+  //   }
+  // })
 
-  const creator = await prisma.creator.findMany({
-    where: {
-      id: num, //space[0].authorId
-    }
-  })
+  // const creator = await prisma.creator.findMany({
+  //   where: {
+  //     id: space[0].authorId,
+  //   }
+  // })
 
   return (
     <main className={styles.main}>
-      <AppContent creatorname={creator[0].name} spacename={space[0].title} link={space[0].link} category={space[0].category} instagram={creator[0].instagram} youtube={creator[0].youtube} website={creator[0].website} twitter={creator[0].twitter} />
+      {/* <AppContent creatorname={creator[0].name} spacename={space[0].title} link={space[0].link} category={space[0].category} instagram={creator[0].instagram} youtube={creator[0].youtube} website={creator[0].website} twitter={creator[0].twitter} /> */}
     </main>
   )
 }
